f9af2b87dcaea9e48f206ac348c9e42dbf13ba4f
davidsnts <david.santos@delage.com.br>
2025-09-04 20:05:44 -0300

    Add swagger

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger.json");
 
 const app = express();
 
@@ -10,6 +12,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get('/', (req, res) => res.send({ 'msg': 'bem vindo a nossa API.' }));
 
